9a0e5c615392eff023abb9c3c8244b2bea3d4e02
JJ <jgg3295@gmail.com>
2017-10-01 15:01:26 -0700

    add game images

--- projectenv/vglproject/static/js/components/game.js
+++ projectenv/vglproject/static/js/components/game.js
@@ -9,14 +9,48 @@ angular.module('main')
 		}, function error(res) {
 		});
 
+	};
+	
+	$scope.clickRun = () => {
+		var arr = $scope.playerInput.split(',');
+		console.log('arr ', arr);
+		var data = [
+		[[], [], [], [], []],
+	  [[], [], [], [], []],
+	  [[], [], [], [], []],
+	  [['X'], [], [], [], []],
+	  [[], [], [], [], []]];
+
+	  var table = angular.element(document.getElementsByClassName('table'))[0];
+	  // console.log('children ', table.getElementsByTagName('TR'));
+	  var rows = table.getElementsByTagName('TR');
+	  for (var i = 0; i < rows.length; i++) {
+	  	cells = rows[i].getElementsByTagName('TD');
+	  	for (var j = 0; j < cells.length; j++) {
+	  		if (JSON.stringify(data[i][j]) === '[]') {
+	  			cells[j].style.backgroundImage = 'url(static/assets/grass.png)';
+	  		} else {
+	  			cells[j].style.backgroundImage = 'url(static/assets/man.png)';
+	  		}
+	  	}
+	  }
+
 		// $http({
 		// 	url: '/api/run/',
-		// 	method: 'GET',
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'X-Requested-With': 'XMLHttpRequest',
+		// 		'Content-Type': 'application/x-www-form-urlencoded'
+		// 	},
+		// 	data: {
+		// 		commands: arr
+		// 	}
 		// }).then(function success(res) {
-		// 	res
+
 		// }, function error(res) {
 		// });
-	};
+
+	}
 
 })
 .directive('game', () => {
